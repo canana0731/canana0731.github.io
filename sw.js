5a04970d47597c7393d30469f26e04ea8fc1cafe
canana0731 <3090048782@qq.com>
2026-07-23 22:07:14 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "guwu-v3";
+const CACHE = "guwu-v4";
 const SHELL = ["/", "/manifest.webmanifest"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
